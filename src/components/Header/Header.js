627778fbb8ab4005c428d8bf3c55b85cf0315444
emon2058 <ashad2058@gmail.com>
2021-09-26 21:18:15 +0600

    update header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,8 @@ const Header = () => {
     return (
         <div className="header">
             <h1>Covid Countries Information</h1>
+            <p>Together, we can slow the spread of COVID-19 by making a conscious effort to keep a physical distance between each other. Physical (social) distance is proven to be one of the most effective ways to reduce the spread of illness during an outbreak.</p>
+            <h1 className="death">Total Death : 4,759,668</h1>
         </div>
     );
 };
