f041bae855552aa37feff2ad0ac97a9a8d41284e
emon2058 <ashad2058@gmail.com>
2021-09-26 20:46:32 +0600

    test components

---
+++ src/components/Header/Info/Info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Cart from '../Country/Cart/Cart';
+import Country from '../Country/Country';
+
+const Info = () => {
+    return (
+        <div>
+            <h1>This is info</h1>
+            <Country></Country>
+            <Cart></Cart>
+        </div>
+    );
+};
+
+export default Info;
